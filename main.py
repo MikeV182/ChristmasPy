53ca1e0093a6e92bf1e833e854e176cf65f08235
MikeV182 <michailvays@gmail.com>
2025-01-12 21:31:49 +0700

    done scrapping pages, stored greetings into a dictionary

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 try:
     from telethon import TelegramClient
-    import asyncio
+    from threading import Thread
     from parser import Parser
-    import traceback, sys, re, requests
+    import traceback, sys, requests, os
     from lxml import html
 
 
@@ -13,17 +13,30 @@ try:
     client = TelegramClient('client', api_id, api_hash)
 
 
-    def scrap_web_page(greetings_dict):
-        url = 'https://pozdravok.com/pozdravleniya/prazdniki/noviy-god/korotkie/'
+    def scrap_web_page(page_id, greetings_dict):
+        url = 'https://pozdravok.com/pozdravleniya/prazdniki/noviy-god/korotkie/' \
+            + str(page_id) + '.htm'
+        print(f'started scrapping page {page_id}:\n\t', url)
         responce = requests.get(url)
         if responce.status_code == 200:
             element_tree = html.fromstring(responce.content)
         else:
             print('a problem occured when sending request to a web page:\n\t', 
                   url, 
-                  f'\n\tstatus code: {responce.status_code}')
-        firts_greeting = element_tree.xpath('//div[@class="content"]/p[1]/text()')
-        print(firts_greeting)
+                  f'\n\t status code: {responce.status_code}')
+            os._exit(1)
+        id = 1
+        while True:
+            selected_greeting = element_tree.xpath(
+                f'//div[@class="content"]/p[{id}]/text()')
+            selected_greeting_id = element_tree.xpath(
+                f'//div[@class="content"]/p[{id}]/@id')
+            if len(selected_greeting) == 0:
+                break
+            else:
+                greetings_dict[selected_greeting_id[0]] = selected_greeting
+                id += 1
+        print(f'page {page_id} status: \033[32m[DONE]\033[0m')
 
 
     async def main():
@@ -32,10 +45,22 @@ try:
         print('Username:', me.username, '\nPhone:', me.phone)
 
         print('\n==== STARTED SCRAPPING THE GREETINGS PAGE ====')
-        scrap_web_page(greetings_dict)
+        # starting from second page because first page has unique url '/', 
+        # but others will have formats like this '/i.htm'
+        i = 2 
+        active_threads = []
+        while i <= 6:
+            thread = Thread(target=scrap_web_page, args=(i, greetings_dict))
+            i += 1
+            active_threads.append(thread)
+            thread.start()
+
+        for thread in active_threads: # waiting for all threads to terminate
+            thread.join()
         print('==== GREETINGS COLLECTED ====')
 
         print('\n==== SENDING GREETIGNS TO ALL MENTIONED USERS ====')
+        
 
 
     with client:
